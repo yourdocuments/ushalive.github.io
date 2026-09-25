b3e02be17b8e5f6f9d52f78242dd37dae54bfd48
yourdocuments <sakhijahanusha@gmail.com>
2026-09-26 01:30:06 +0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,52 +3,31 @@
 // Firebase Configuration
 // =========================================================
 
-// Firebase SDK
+// Firebase Core
 import { initializeApp } from
   "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
-import {
-  getAuth
-} from
+// Firebase Authentication
+import { getAuth } from
   "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-import {
-  getFirestore
-} from
+// Firebase Firestore
+import { getFirestore } from
   "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 
 // =========================================================
 // FIREBASE CONFIG
 // =========================================================
-//
-// IMPORTANT:
-// নিচের configuration আপনার Firebase Console-এর
-// Web App configuration দিয়ে replace করতে হবে.
-//
-// Firebase Console:
-// Project Settings
-// → General
-// → Your apps
-// → Web app
-// → SDK setup and configuration
-// → Config
-//
 
 const firebaseConfig = {
-
-  apiKey: "PASTE_YOUR_FIREBASE_API_KEY",
-
-  authDomain: "PASTE_YOUR_PROJECT_ID.firebaseapp.com",
-
-  projectId: "PASTE_YOUR_PROJECT_ID",
-
-  storageBucket: "PASTE_YOUR_PROJECT_ID.firebasestorage.app",
-
-  messagingSenderId: "PASTE_YOUR_MESSAGING_SENDER_ID",
-
-  appId: "PASTE_YOUR_APP_ID"
-
+  apiKey: "AIzaSyDyY-R2rCliamC9cxJOAFATOnnDJ0t30dA",
+  authDomain: "ushaliveai.firebaseapp.com",
+  projectId: "ushaliveai",
+  storageBucket: "ushaliveai.firebasestorage.app",
+  messagingSenderId: "686319624480",
+  appId: "1:686319624480:web:5afe9c39aa8f2af39b7745",
+  measurementId: "G-HMBS0H7LTT"
 };
 
 
